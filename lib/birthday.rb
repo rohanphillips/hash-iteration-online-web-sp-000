ffba2c045b08e18ab9c0c86c7f21adf796520e7c
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 16:26:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each {|kids_name, age| age <= 12 ? puts "Happy Birthday #{kids_name}! You are now #{age} years old!" : puts "You are too old for this."}
+  birthday_kids.each {|kids_name, age| puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12 }
 
   end
 end
